76e8d99eff3ec0a04e8807debab8b86d7f7e8414
Your Name <angelicrf@gmail.com>
2024-03-24 12:34:35 -0700

    env-mdf-4

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === "production"
-      ? "/your-repo-name/" // Replace 'your-repo-name' with your actual repository name
-      : "/",
+  base: process.env.NODE_ENV === "production" ? "/angy-find-template/" : "/",
   plugins: [react()],
 });
